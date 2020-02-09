133e74c5f82ea9ab7f088bfb3ffd4bcd7296fb7d
Aleksandr Sidorenko <asidorenko@simdaq.com>
2020-02-09 16:18:14 +1000

    Logger via lower level API (take vs takeEvery)

--- src/state/sagas/logger.js
+++ src/state/sagas/logger.js
@@ -1,10 +1,11 @@
-import { select, takeEvery } from 'redux-saga/effects';
+import { select, take } from 'redux-saga/effects';
 
 export default function* watchAndLog() {
-  yield takeEvery('*', function* logger(action) {
+  while (true) {
+    const action = yield take('*');
     const state = yield select();
 
     // eslint-disable-next-line no-console
     console.log(JSON.stringify({ action, state }, null, 2));
-  });
+  }
 }
